12cbabf847e2b298111c5b65974ec0e39c1e8c95
VeronicaNonxuba <veronical.nonxuba@gmail.com>
2024-11-13 17:57:00 +0200

    changed color them and also added more pages

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ const navLinks = document.querySelectorAll("nav a");
 
 navLinks.forEach((link) => {
   link.addEventListener("click", function (event) {
-    event.preventDefault();
-    const targetId = this.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-    targetElement.scrollIntoView({ behavior: "smooth" });
+    // No smooth scrolling needed here.
+    window.location.href = this.getAttribute("href");
   });
 });
+
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     const targetId = this.getAttribute("href");
+//     const targetElement = document.querySelector(targetId);
+//     targetElement.scrollIntoView({ behavior: "smooth" });
+//   });
+// });
