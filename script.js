dc835c1843c64a28147ccef2c42b1fe2cc56b94b
VeronicaNonxuba <veronical.nonxuba@gmail.com>
2024-11-17 15:16:05 +0200

    revert to previous commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const requestUrl = "https://formsubmit.co/veronical.nonxuba@gmail.com";
 // Example: Smooth scrolling
 const navLinks = document.querySelectorAll("nav a");
 
@@ -17,26 +16,3 @@ navLinks.forEach((link) => {
 //     targetElement.scrollIntoView({ behavior: "smooth" });
 //   });
 // });
-
-function sendData() {
-  let formBody = [
-    "name=" + encodeURIComponent(document.getElementById("name").value),
-    "email=" + encodeURIComponent(document.getElementById("email").value),
-    "message=" + encodeURIComponent(document.getElementById("message").value),
-    "next=" + encodeURIComponent(document.getElementsByName("_next").value),
-  ].join("&");
-
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", requestUrl);
-
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      window.location = "./thanks.html";
-      return;
-    }
-    alert("An error occured");
-  };
-
-  xhr.send(formBody);
-}
