58a014a5b73afffe7c5dc6ab3734a94b41e4b21a
VeronicaNonxuba <veronical.nonxuba@gmail.com>
2024-11-17 15:05:03 +0200

    try to send email via js to avoid having email on html

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// You can add JavaScript code here to handle:
-// - Smooth scrolling to sections when clicking navigation links
-// - Form validation and submission
-// - Any other interactive elements you want to add.
-
+const requestUrl = "https://formsubmit.co/veronical.nonxuba@gmail.com";
 // Example: Smooth scrolling
 const navLinks = document.querySelectorAll("nav a");
 
@@ -21,3 +17,26 @@ navLinks.forEach((link) => {
 //     targetElement.scrollIntoView({ behavior: "smooth" });
 //   });
 // });
+
+function sendData() {
+  let formBody = [
+    "name=" + encodeURIComponent(document.getElementById("name").value),
+    "email=" + encodeURIComponent(document.getElementById("email").value),
+    "message=" + encodeURIComponent(document.getElementById("message").value),
+    "next=" + encodeURIComponent(document.getElementsByName("_next").value),
+  ].join("&");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", requestUrl);
+
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      window.location = "./thanks.html";
+      return;
+    }
+    alert("An error occured");
+  };
+
+  xhr.send(formBody);
+}
